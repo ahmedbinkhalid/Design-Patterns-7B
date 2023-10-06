de05293221fb656e0660d600e30602165767fb87
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-10-06 10:27:15 +0500

    Decorator Pattern

---
+++ Decorator/Example1/src/ShapeDecorator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FA20-BSE-062
+ */
+public abstract class ShapeDecorator implements Shape {
+   protected Shape decoratedShape;
+
+   public ShapeDecorator(Shape decoratedShape){
+      this.decoratedShape = decoratedShape;
+   }
+
+   public void draw(){
+      decoratedShape.draw();
+   }	
+}
