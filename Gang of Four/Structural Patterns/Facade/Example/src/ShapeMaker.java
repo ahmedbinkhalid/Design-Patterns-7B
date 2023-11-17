7ad75864c19975b971320ca34315615c7cd46694
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-11-17 10:15:22 +0500

    Facade Pattern

---
+++ Gang of Four/Structural Patterns/Facade/Example/src/ShapeMaker.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FA20-BSE-062
+ */
+public class ShapeMaker {
+   private Shape circle;
+   private Shape rectangle;
+   private Shape square;
+
+   public ShapeMaker() {
+      circle = new Circle();
+      rectangle = new Rectangle();
+      square = new Square();
+   }
+
+   public void drawCircle(String... options){
+      circle.draw(options);
+   }
+   public void drawRectangle(){
+      rectangle.draw();
+   }
+   public void drawSquare(){
+      square.draw();
+   }
+}
