e36f4d8fa3fdd01091b5b6ea4f4f239c82f480a9
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-11-24 08:59:05 +0500

    Update MediaPlayer.java

--- Gang of Four/Structural Patterns/Adapter/TutorialPointExample/MediaPlayer.java
+++ Gang of Four/Structural Patterns/Adapter/TutorialPointExample/MediaPlayer.java
@@ -6,7 +6,7 @@ package GOF.StructuralPattern.Adapter.TutorialPointExample;
 
 /**
  *
- * @author shafq
+ * @author ahmed
  */
 public interface MediaPlayer {
    public void play(String audioType, String fileName);
