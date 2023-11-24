6b275c27c39a788f57818c5b3614b76e020b4411
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-11-24 09:40:52 +0500

    Update SellStock.java

--- Gang of Four/Behavioral Patterns/Command/TutorialPointExample/SellStock.java
+++ Gang of Four/Behavioral Patterns/Command/TutorialPointExample/SellStock.java
@@ -7,7 +7,7 @@ package GOF.BehavioralPattern.Command.TutorialPointExample;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 public class SellStock implements Order {
    private Stock abcStock;
