77384e1a78985f557d28290bd58ac43c4d28736a
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-01 10:11:54 +0500

    Update HOD.java

--- Gang of Four/Structural Patterns/Composite/LabTask/HOD.java
+++ Gang of Four/Structural Patterns/Composite/LabTask/HOD.java
@@ -7,7 +7,7 @@ package GOF.StructuralPattern.Composite.LabTask;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 abstract class HOD extends BaseEmployee {
     public HOD(String name, double salary) {
