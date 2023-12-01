fd094f47ac18d126afb731cb648b72ccb45576f8
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-01 10:10:59 +0500

    Update DOO.java

--- Gang of Four/Structural Patterns/Composite/LabTask/DOO.java
+++ Gang of Four/Structural Patterns/Composite/LabTask/DOO.java
@@ -7,7 +7,7 @@ package GOF.StructuralPattern.Composite.LabTask;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 abstract class DOO extends BaseEmployee {
     public DOO(String name, double salary) {
