b7cd1502f8c1fcf89b720fcd2187096cdbb832f9
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-01 10:11:16 +0500

    Update Director.java

--- Gang of Four/Structural Patterns/Composite/LabTask/Director.java
+++ Gang of Four/Structural Patterns/Composite/LabTask/Director.java
@@ -7,7 +7,7 @@ package GOF.StructuralPattern.Composite.LabTask;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 abstract class Director extends BaseEmployee {
     public Director(String name, double salary) {
