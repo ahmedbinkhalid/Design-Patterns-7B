1c8aa2bbce5cf31f3becece3ac29a14ce2036bac
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-01 10:13:25 +0500

    Update CompositePatternDemo.java

--- Gang of Four/Structural Patterns/Composite/TutorialPointExample/CompositePatternDemo.java
+++ Gang of Four/Structural Patterns/Composite/TutorialPointExample/CompositePatternDemo.java
@@ -7,7 +7,7 @@ package GOF.StructuralPattern.Composite.TutorialPointExample;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 public class CompositePatternDemo {
    public static void main(String[] args) {
