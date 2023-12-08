8b2ac483bb41b60bb2fd8da168733f5e4140c107
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-08 09:06:28 +0500

    Refactoring-Guru Example

---
+++ Gang of Four/Structural Patterns/State/Refacturing-Guru Example/src/State.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FA20-BSE-062
+ */
+public abstract class State {
+    Player player;
+
+    /**
+     * Context passes itself through the state constructor. This may help a
+     * state to fetch some useful context data if needed.
+     */
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
