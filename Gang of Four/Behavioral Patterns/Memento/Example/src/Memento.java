d140add456bd0a7c97018707ec765ee6bd37100b
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-15 10:48:55 +0500

    Memento

---
+++ Gang of Four/Behavioral Patterns/Memento/Example/src/Memento.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FA20-BSE-062
+ */
+public class Memento {
+    private String backup;
+    private Editor editor;
+
+    public Memento(Editor editor) {
+        this.editor = editor;
+        this.backup = editor.backup();
+    }
+
+    public void restore() {
+        editor.restore(backup);
+    }
+}
