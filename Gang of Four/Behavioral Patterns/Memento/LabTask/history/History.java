60270ec6b355099684236523373831ea9602bd2a
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-15 10:56:18 +0500

    Update History.java

--- Gang of Four/Behavioral Patterns/Memento/LabTask/history/History.java
+++ Gang of Four/Behavioral Patterns/Memento/LabTask/history/History.java
@@ -7,7 +7,7 @@ package GOF.BehavioralPattern.Memento.LabTask.history;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 import GOF.BehavioralPattern.Memento.LabTask.commands.Command;
 
