e9d52432a9601fe97e5861046d61e825f4882a93
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-22 10:22:36 +0500

    Update Geometery.java

--- Gang of Four/Creational Patterns/FactoryMethod/FactoryMethodFA20/Geometery.java
+++ Gang of Four/Creational Patterns/FactoryMethod/FactoryMethodFA20/Geometery.java
@@ -7,7 +7,7 @@ package GOF.CreationalPattern.FactoryMethod.FactoryMethodFA20;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 public abstract class Geometery {
 	public abstract Shape createShape();
