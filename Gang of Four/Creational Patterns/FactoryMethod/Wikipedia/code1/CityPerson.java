44872736a82e3fdee0a9c62476e3b903158c9f7d
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-12-22 10:28:31 +0500

    Update CityPerson.java

--- Gang of Four/Creational Patterns/FactoryMethod/Wikipedia/code1/CityPerson.java
+++ Gang of Four/Creational Patterns/FactoryMethod/Wikipedia/code1/CityPerson.java
@@ -7,7 +7,7 @@ package GOF.CreationalPattern.FactoryMethod.Wikipedia.code1;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-062
  */
 // Concrete class for CityPerson
 public class CityPerson implements IPerson {
